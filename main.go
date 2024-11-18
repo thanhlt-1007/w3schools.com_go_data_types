6ff7cf9eab1d374505e7d28dd0d2147cf2c1590c
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-18 22:27:07 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World!")
+    /*
+    Go Data Types
+    -   bool
+    -   Numeric
+    -   string
+    */
+
+    var a bool = true
+    fmt.Println("Boolean: ", a)
+
+    var b int = 5
+    fmt.Println("Integer: ", b)
+
+    var c float32 = 3.14
+    fmt.Println("Float: ", c)
+
+    var d string = "Hi!"
+    fmt.Println("String: ", d)
 }
